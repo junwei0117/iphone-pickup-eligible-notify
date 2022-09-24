cd94ef857da6ec627f8de612028f97554ef512d0
junwei0117 <v860117@gmail.com>
2022-09-24 10:03:39 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import TelegramBot from "node-telegram-bot-api";
+import {targets, telegramAPIToken, chatID} from './config.js'
+
+const bot = new TelegramBot(telegramAPIToken, {polling: true});
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const start = async () => {
+  while(true) {
+    targets.forEach(async model => {
+      try {
+        const storeResponse = await axios.get(`https://www.apple.com/tw/shop/fulfillment-messages?pl=true&mts.0=regular&mts.1=compact&parts.0=${model}&searchNearby=true&&store=R713`)
+        storeResponse.data.body.content.pickupMessage.stores.forEach(store => {
+          console.log(`${model}: ${store.storeName}: ${store.partsAvailability[model].pickupDisplay}`)
+          if (store.partsAvailability[model].pickupDisplay == "available") {
+            bot.sendMessage(chatID, model);
+          }
+        })
+      } catch (err) {
+        console.log(err.data)
+      }
+    })
+
+    await wait(5000)
+  }
+}
+
+start()
